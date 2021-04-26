ba28ba82debdc49edf66cc192619757c537b7cb5
respectorest1 <vianulhag@gmail.com>
2021-04-26 07:29:11 +0700

    Ser.Com Virtual Terminal

---
+++ Ser.Com Virtual Terminal/VIRTUAL_TERMINAL_1.ino
@@ -0,0 +1,17 @@
+
+void setup()
+{
+Serial.begin(9600);
+}
+void loop()
+{
+Serial.println("A");
+Serial.println("1");
+Serial.println("2");
+Serial.println("3");
+Serial.println("B");
+Serial.println("4");
+Serial.println("5");
+Serial.println("6");
+Serial.end();
+}
